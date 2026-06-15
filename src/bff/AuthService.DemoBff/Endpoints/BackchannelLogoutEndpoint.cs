710d51a58015e21731adae9060aad3c2ac5df7fd
BigB <128973794+Bishal0416@users.noreply.github.com>
2026-06-15 17:46:54 +0530

    bug fix in backchannel logout

--- src/bff/AuthService.DemoBff/Endpoints/BackchannelLogoutEndpoint.cs
+++ src/bff/AuthService.DemoBff/Endpoints/BackchannelLogoutEndpoint.cs
@@ -61,7 +61,7 @@ public static class BackchannelLogoutEndpoint
 
             var validationParams = new TokenValidationParameters
             {
-                ValidIssuer = authority + "/",
+                ValidIssuer = authority.TrimEnd('/') + "/",
                 ValidAudience = clientId,
                 IssuerSigningKeys = oidcConfig.SigningKeys,
                 ValidateIssuer = true,
